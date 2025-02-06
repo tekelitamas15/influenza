37fc5dd5ac589912ff084c53758f737ffe072a03
tekelitamas15 <52209030+tekelitamas15@users.noreply.github.com>
2025-02-06 13:51:28 +0100

    Update SEIRHETmodel.py

--- SEIRHETmodel.py
+++ SEIRHETmodel.py
@@ -1,4 +1,6 @@
 from scipy import integrate
+import numpy as np
+from scipy import optimize
 
 
 def seirhet_model(y, x, beta, gamma, alpha, p, N):
@@ -18,14 +20,14 @@ def seirhet_model(y, x, beta, gamma, alpha, p, N):
         tuple: Derivatives of the compartments (dS/dt, dE/dt, dI/dt, dR/dt).
     """
     N = sum(y)  # Ensure the total population size remains consistent.
-    S = -beta * pow(y[0], 1 + (1 / p)) * y[2] / N
-    E = beta * pow(y[0], 1 + (1 / p)) * y[2] / N - alpha * y[1]
+    S = -beta * pow((y[0]/N), 1 + (1 / p)) * y[2]
+    E = beta * pow((y[0]/N), 1 + (1 / p)) * y[2] - alpha * y[1]
     I = alpha * y[1] - gamma * y[2]
     R = gamma * y[2]
     return S, E, I, R
 
 
-def solve_SEIRhet(x, beta, gamma, alpha, p, E0, N):
+def solve_SEIRhet(x, beta, gamma, alpha, p, E0, R0, N, ur = 3.8):
     """
     Solves the SEIR-HET model using numerical integration.
 
@@ -41,10 +43,12 @@ def solve_SEIRhet(x, beta, gamma, alpha, p, E0, N):
     Returns:
         ndarray: Sum of the Exposed (E) and Infectious (I) compartments at each time point.
     """
-    S0 = N - E0  # Initial number of susceptible individuals.
-    I0 = 0       # Initial number of infectious individuals.
-    R0 = 0       # Initial number of recovered individuals.
+    I0 = E0
+    S0 = N - E0 - I0 - R0
 
-    # Solve the ODE system for the E and I compartments
-    results = integrate.odeint(seirhet_model, (S0, E0, I0, R0), x, args=(beta, gamma, alpha, p, N), atol=1e-3, rtol=1e-3)
-    return results[:, 1] + results[:, 2]  # Return the sum of E and I compartments.
+    # Solve the ODE system
+    results = integrate.odeint(seirhet_model, (S0, E0, I0, R0), x, args=(beta, gamma, alpha, p, N))
+    diff = [ur * E0]
+    for i in range(1, len(results[:, 3])):
+        diff.append(results[:, 3][i] - results[:, 3][i - 1])
+    return diff
