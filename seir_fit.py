a799579ffad024e5f2c6f955409a505494f6db90
tekelitamas15 <52209030+tekelitamas15@users.noreply.github.com>
2025-02-06 13:52:19 +0100

    Update seir_fit.py

--- seir_fit.py
+++ seir_fit.py
@@ -18,15 +18,18 @@ def seir_fit(data_dates, data_cases, N, initial_guess):
     """
 
     # Define a function for SEIR fitting
-    def wrapped_model(x, beta, gamma, alpha, E0):
-        return solve_SEIR(x, beta, gamma, alpha, E0, N)
+    def wrapped_model(x, beta, gamma, alpha, E0, R0):
+        return solve_SEIR(x, beta, gamma, alpha, E0, R0, N, ur=3.8)
 
     popt, _ = optimize.curve_fit(
             wrapped_model, data_dates, data_cases, p0=initial_guess,
-            bounds=((0, 0, 0, 0), (np.inf, 7, 7, N))
+            bounds=((0, 3.9, 3.9, 0, 0), (np.inf, 4.1, 4.1, N, N))
         )
     fitted_values = wrapped_model(data_dates, *popt)
     return fitted_values, popt
+
+
+
 
 
 
